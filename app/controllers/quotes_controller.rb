5bc124e9728a3975461a3adebee7050dc2d90958
Syed Muhammad Ali Gardezi <smaligardezi@gmail.com>
2017-05-16 13:50:10 +0000

    Quotesfucntion

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -6,9 +6,12 @@ def new
 	    @quote = Quote.new
   end
 
- def create
-    Quote.create(quote_params)
-        redirect_to root_path
+   def create
+    @quote = Quote.create(quote_params)
+    if @quote.invalid?
+      flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+    end
+    redirect_to root_path
   end
 
   private
